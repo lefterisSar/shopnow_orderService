6c0f7128cb70b6dea9e8e2bf3aaf71fd5baec37c
Lefteris Saridakis <saridakiselef@hotmail.com>
2025-04-13 17:30:45 +0300

    Initial commit .

---
+++ src/main/java/com/shopnow/order_service/ShopnowOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.shopnow.order_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopnowOrderServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopnowOrderServiceApplication.class, args);
+    }
+
+}
