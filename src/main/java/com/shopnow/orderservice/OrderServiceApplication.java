ba27070cbfa37b026508d4e5179f62c5a7f33900
Lefteris Saridakis <saridakiselef@hotmail.com>
2025-04-17 18:48:08 +0300

    Integrate dotenv for environment variable management.
    Added dotenv-java dependency to manage environment variables securely. Updated `OrderServiceApplication` to load properties from a `.env` file and set system properties. Enhanced `DatasourceProperties` with a `@PostConstruct` method to verify environment variables at startup.

--- src/main/java/com/shopnow/orderservice/OrderServiceApplication.java
+++ src/main/java/com/shopnow/orderservice/OrderServiceApplication.java
@@ -2,6 +2,7 @@ package com.shopnow.orderservice;
 
 import com.shopnow.orderservice.config.DatasourceProperties;
 import com.shopnow.orderservice.config.JwtProperties;
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -11,6 +12,11 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 public class OrderServiceApplication {
 
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure().load();
+        System.setProperty("SPRING_DATASOURCE_URL", dotenv.get("SPRING_DATASOURCE_URL"));
+        System.setProperty("SPRING_DATASOURCE_USERNAME", dotenv.get("SPRING_DATASOURCE_USERNAME"));
+        System.setProperty("SPRING_DATASOURCE_PASSWORD", dotenv.get("SPRING_DATASOURCE_PASSWORD"));
+        System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
         SpringApplication.run(OrderServiceApplication.class, args);
     }
 
--- src/main/java/com/shopnow/orderservice/config/DatasourceProperties.java
+++ src/main/java/com/shopnow/orderservice/config/DatasourceProperties.java
@@ -1,5 +1,6 @@
 package com.shopnow.orderservice.config;
 
+import jakarta.annotation.PostConstruct;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
@@ -11,4 +12,12 @@ public class DatasourceProperties {
     private String url;
     private String username;
     private String password;
+
+    @PostConstruct
+    public void checkEnv() {
+        System.out.println("SPRING_DATASOURCE_PASSWORD (System): " + System.getProperty("SPRING_DATASOURCE_PASSWORD"));
+        System.out.println("SPRING_DATASOURCE_USERNAME (System): " + System.getProperty("SPRING_DATASOURCE_USERNAME"));
+        System.out.println("SPRING_DATASOURCE_URL (System): " + System.getProperty("SPRING_DATASOURCE_URL"));
+    }
+
 }
